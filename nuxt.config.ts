ffc66300c8afb8f8d392124bb2850f21dae6c5c9
Maartje Hagens <maartjevandriesten@live.nl>
2025-08-16 19:36:55 +0200

    fout oplossen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,6 @@ export default defineNuxtConfig({
       apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://127.0.0.1:8000/api",
     },
   },
-  css: ["../assets/styles.css"],
+  css: ["~/assets/styles.css"],
   compatibilityDate: "2025-08-16",
 });
